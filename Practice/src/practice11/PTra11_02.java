0ffe2bbfc4bd033022b29eac7c0622d5500bdb22
USER-PC\USER <takuto.tagawa.likeit@gmail.com>
2018-09-18 20:37:33 +0900

    11-3

--- Practice/src/practice11/PTra11_02.java
+++ Practice/src/practice11/PTra11_02.java
@@ -10,11 +10,18 @@ public class PTra11_02 {
 	public static void main(String[] args) {
 
 		// ★ ReadFileClassのクラスメソッドreadBookDataFile()を使い、本情報を取得します
-
+		Book[] booklist = FileReaderClass.readBookDataFile();
 
 		System.out.println("探したい本のタイトル（又はその一部）を入力してください");
 
 		// ★ ユーザが入力した文字列が、本情報のタイトル一部に含まれていた場合は、その本情報を出力してください
+		String scanner = new java.util.Scanner(System.in).nextLine();
+		String kensaku = scanner;
 
+		for (Book value : booklist) {
+			if (value.title.contains(kensaku)) {
+				System.out.println(value.dispBookInfo());
+			}
+		}
 	}
 }

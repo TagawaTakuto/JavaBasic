043e91184620540459c85fbd39836e72cd5ee312
USER-PC\USER <takuto.tagawa.likeit@gmail.com>
2018-09-25 18:42:00 +0900

    16-1

--- Practice/src/practice15/ptra15/PTra15_02.java
+++ Practice/src/practice15/ptra15/PTra15_02.java
@@ -16,8 +16,11 @@ public class PTra15_02 {
 
 		// roomに格納されたJava2MonthインスタンスのgetCurriculumメソッドを呼び出してください
 
+		((Java2Month)room).getCurriculum();
 
 		// 取得したカリキュラム情報を全て出力してください
-
+		for(String String : ((Java2Month)room).getCurriculum()) {
+			System.out.println(String);
+		}
 	}
 }
